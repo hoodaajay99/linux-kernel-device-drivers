273dd44d7105b11311f6338e304ca6b2428639d5
Ajay Hooda <hoodaajay@gmail.com>
2020-01-15 15:05:24 +0530

    customize site

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -21,129 +21,135 @@ class Footer extends React.Component {
     return baseUrl + (language ? `${language}/` : '') + doc;
   }
 
-  // render() {
-  //   return (
-  //     <footer className="nav-footer" id="footer">
-  //       <section className="sitemap">
-  //         <a href={this.props.config.baseUrl} className="nav-home">
-  //           {this.props.config.footerIcon && (
-  //             <img
-  //               src={this.props.config.baseUrl + this.props.config.footerIcon}
-  //               alt={this.props.config.title}
-  //               width="66"
-  //               height="58"
-  //             />
-  //           )}
-  //         </a>
-  //         <div>
-  //           <h5>Courses</h5>
-  //           <a href={this.docUrl('00-Course-Introduction/course-introduction.html', this.props.language)}>
-  //             C Programming
-  //           </a>
-  //           {/* <a href={this.docUrl('doc2.html', this.props.language)}>
-  //             Guides (or other categories)
-  //           </a>
-  //           <a href={this.docUrl('doc3.html', this.props.language)}>
-  //             API Reference (or other categories)
-  //           </a> */}
-  //         </div>
-  //         <div>
-  //           <h5>Community</h5>
-  //           {/* <a href={this.pageUrl('users.html', this.props.language)}>
-  //             User Showcase
-  //           </a> */}
-  //           <a
-  //             href="https://stackoverflow.com/questions/tagged/"
-  //             target="_blank"
-  //             rel="noreferrer noopener">
-  //             Stack Overflow
-  //           </a>
-  //           {/* <a href="https://discordapp.com/">Project Chat</a> */}
-  //           <a
-  //             href="https://twitter.com/"
-  //             target="_blank"
-  //             rel="noreferrer noopener">
-  //             Twitter
-  //           </a>
-  //         </div>
-  //         <div>
-  //           <h5>Resources</h5>
-  //           <a href={`${this.props.config.baseUrl}blog`}>Blog</a>
-  //           <a href="https://github.com/hoodaajay99">GitHub</a>
-  //           <a
-  //             className="github-button"
-  //             href={this.props.config.repoUrl}
-  //             data-icon="octicon-star"
-  //             data-count-href="hoodaajay99/c_programming/stargazers"
-  //             data-show-count="true"
-  //             data-count-aria-label="# stargazers on GitHub"
-  //             aria-label="Star this project on GitHub">
-  //             Star
-  //           </a>
-  //           {this.props.config.twitterUsername && (
-  //             <div className="social">
-  //               <a
-  //                 href={`https://twitter.com/${
-  //                   this.props.config.twitterUsername
-  //                 }`}
-  //                 className="twitter-follow-button">
-  //                 Follow @{this.props.config.twitterUsername}
-  //               </a>
-  //             </div>
-  //           )}
-  //           {this.props.config.facebookAppId && (
-  //             <div className="social">
-  //               <div
-  //                 className="fb-like"
-  //                 data-href={this.props.config.url}
-  //                 data-colorscheme="dark"
-  //                 data-layout="standard"
-  //                 data-share="true"
-  //                 data-width="225"
-  //                 data-show-faces="false"
-  //               />
-  //             </div>
-  //           )}
-  //         </div>
-  //       </section>
-
-  //       <a
-  //         href="https://techskillo.com/"
-  //         target="_blank"
-  //         rel="noreferrer noopener"
-  //         className="fbOpenSource">
-  //         <img
-  //           src={`${this.props.config.baseUrl}img/oss_logo.png`}
-  //           alt="Facebook Open Source"
-  //           width="170"
-  //           height="45"
-  //         />
-  //       </a>
-  //       <section className="copyright">{this.props.config.copyright}</section>
-  //     </footer>
-  //   );
-  // }
-
   render() {
     return (
       <footer className="nav-footer" id="footer">
-        <a
-          href="https://techskillo.com/"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="fbOpenSource">
-          <img
-            src={`${this.props.config.baseUrl}img/oss_logo.png`}
-            alt="Facebook Open Source"
-            width="170"
-            height="45"
-          />
-        </a>
+        <section className="sitemap">
+          <div>
+            <h5>TECH SKILLO</h5>
+            <a href={"/"}>
+              Courses
+            </a>
+            <a href={"/"}>
+              Bundles
+            </a>
+            <a href={"/"}>
+              Android App
+            </a>
+            <a href={"/"}>
+              iOS App
+            </a>
+            <a href={"/"}>
+              Feature Request
+            </a>
+            <a href={"/"}>
+              Interships
+            </a>
+          </div>
+
+          <div>
+            <h5>COMPANY</h5>
+            <a href={"/"}>
+              About Us
+            </a>
+            <a href={"/"}>
+              Terms
+            </a>
+            <a href={"/"}>
+              Privacy
+            </a>
+            <a href={"/"}>
+              Pricing Policy
+            </a>
+            <a href={"/"}>
+              SiteMap
+            </a>
+            <a href={"/"}>
+              Contact Us
+            </a>
+          </div>          
+
+          <div>
+            <h5>COURSES</h5>
+            <a href={"/"}>
+              C Programming
+            </a>
+            <a href={"/"}>
+              Linux Kernel
+            </a>
+            <a href={"/"}>
+              Device Drivers
+            </a>
+            <a href={"/"}>
+              Angular
+            </a>
+            <a href={"/"}>
+              ionic Framework
+            </a>
+            <a href={"/"}>
+              More Courses
+            </a>
+          </div>
+
+          <div>
+            <h5>More Links</h5>
+
+            <a href={"/"}>
+              Careers
+            </a>
+
+            <a href={"/"}>
+              Partners
+            </a>                        
+
+            <a href="https://github.com/hoodaajay99">GitHub</a>
+            <a
+              className="github-button"
+              href={this.props.config.repoUrl}
+              data-icon="octicon-star"
+              data-count-href="/facebook/docusaurus/stargazers"
+              data-show-count="true"
+              data-count-aria-label="# stargazers on GitHub"
+              aria-label="Star this project on GitHub">
+              Star
+            </a>
+
+            <a
+              href="https://stackoverflow.com/questions/tagged/"
+              target="_blank"
+              rel="noreferrer noopener">
+              Stack Overflow
+            </a>
+
+            {this.props.config.twitterUsername && (
+              <div className="social">
+                <a
+                  href={`https://twitter.com/${this.props.config.twitterUsername}`}
+                  className="twitter-follow-button">
+                  Follow @{this.props.config.twitterUsername}
+                </a>
+              </div>
+            )}
+            {this.props.config.facebookAppId && (
+              <div className="social">
+                <div
+                  className="fb-like"
+                  data-href={this.props.config.url}
+                  data-colorscheme="dark"
+                  data-layout="standard"
+                  data-share="true"
+                  data-width="225"
+                  data-show-faces="false"
+                />
+              </div>
+            )}
+          </div>
+        </section>
+
         <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );
   }
-
 }
 
 module.exports = Footer;
